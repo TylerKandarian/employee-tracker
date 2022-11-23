4d3e707c47e7aaad8456b9d6cfb9f25ed986c474
tylerkandarian <tyler.kand2@gmail.com>
2022-11-23 14:22:14 -0500

    added first prompt

---
+++ server.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+
+// mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    // your port
+    port: 3306,
+    // your username
+    user: 'root',
+    // your password
+    password: 'PlacePassWordHere',
+    database: 'employeesDB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    firstPrompt();
+});
